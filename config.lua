f9131f71b78614a08524d365051c10a66ae61f4a
Louis Williams <kossy25@gmail.com>
2012-08-04 00:41:30 +0100

    Started Config file

---
+++ config.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+    t.title = "EGO" 
+    t.author = "BaconGameJam02 - Nonameghost & Kossy" 
+    t.url = 'https://github.com/kossy/BaconGameJam02-EGO'
+    t.identity = nil            -- The name of the save directory (string)
+    t.version = "0.8.0"
+    t.screen.width = 1024
+    t.screen.height = 768
+	t.screen.fullscreen = false
+end
