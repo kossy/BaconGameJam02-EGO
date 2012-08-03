c5bbede52a7f6656497604a482d9b3328907768d
Louis Williams <kossy25@gmail.com>
2012-08-04 00:59:46 +0100

    Added fonts

--- main.lua
+++ main.lua
@@ -1,8 +1,9 @@
 function love.load()
-
+	font = love.graphics.newFont("Fipps.otf", 16)
 end 
 
 
 function love.draw()
-
+	love.graphics.setFont(font)
+	love.graphics.print("EGO Ver 0.2" , 10, 10)
 end
