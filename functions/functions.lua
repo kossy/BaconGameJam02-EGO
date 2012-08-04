59c8418f2f83388faf39f72aed135c8d637e7a82
Louis Williams <kossy25@gmail.com>
2012-08-04 02:56:22 +0100

    Added Menu system

---
+++ functions/functions.lua
@@ -0,0 +1,3 @@
+function chicken()
+	love.graphics.print("Works" , 100, 100)
+end
--- main.lua
+++ main.lua
@@ -1,14 +1,30 @@
+require "functions/functions"
 function love.load()
 	font14 = love.graphics.newFont("Fipps.otf", 14)	
 	font20 = love.graphics.newFont("Fipps.otf", 20)
 	image = love.graphics.newImage("Buttion.png")
+	mode = "Start"
 end 
 
 
 function love.draw()
 	love.graphics.setFont(font14)
 	love.graphics.print("EGO Ver 0.2" , 10, 10)
-	love.graphics.draw(image, 600, 100)
-	love.graphics.setFont(font20)
-	love.graphics.print("Start Press S", 650, 130)
+	
+	if mode == "Start"  then
+  --	love.graphics.draw(image, 600, 100)
+		love.graphics.setFont(font20)
+		love.graphics.print("Start , Press Enter", 650, 130)
+
+	elseif Smode ~= "Start" and mode ~= "Game" then
+		love.graphics.print("Mode Not Set", 650, 130)
+	end
+	function love.keypressed(key)
+		if key == "return" then
+			mode = "Game"
+			love.graphics.print("Game On!", 650, 130)
+		end
+	end
+		
+
 end
