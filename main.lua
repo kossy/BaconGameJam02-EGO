dea53f309919ca54e9dfbf80723e0a2efba4a01e
Louis Williams <kossy25@gmail.com>
2012-08-04 01:30:22 +0100

    Added buttions

--- main.lua
+++ main.lua
@@ -1,9 +1,14 @@
 function love.load()
-	font = love.graphics.newFont("Fipps.otf", 14)
+	font14 = love.graphics.newFont("Fipps.otf", 14)	
+	font20 = love.graphics.newFont("Fipps.otf", 20)
+	image = love.graphics.newImage("Buttion.png")
 end 
 
 
 function love.draw()
-	love.graphics.setFont(font)
+	love.graphics.setFont(font14)
 	love.graphics.print("EGO Ver 0.2" , 10, 10)
+	love.graphics.draw(image, 600, 100)
+	love.graphics.setFont(font20)
+	love.graphics.print("Start Press S", 650, 130)
 end
