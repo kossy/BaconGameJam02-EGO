3fee90e2fc1022004d4e681e663345c7159a8025
Neil Sveri <nxsveri@gmail.com>
2012-08-06 11:58:09 -0400

    Cleaned up code, manually merged
    Work from here from now on.

--- game.lua
+++ game.lua
@@ -2,12 +2,44 @@ require "person"
 require "middleclass/middleclass"
 
 function startgame()
+	
+	scanlines = love.graphics.newImage("media/scanlines.png")
+	world = love.physics.newWorld( 0, 0)
+	
+	border = {}
+	
+	--top = love.physics.newEdgeShape( 110, 100, 930, 100 )
+	border.top = {}
+	border.top.body = love.physics.newBody(world, 520, 95, "static")
+	border.top.shape = love.physics.newRectangleShape(820, 10)
+	border.top.fixture = love.physics.newFixture(border.top.body, border.top.shape, 10) -- A higher density gives it more mass.
 
-	player = love.graphics.newImage("media/player.png")
+	--bottom = love.physics.newEdgeShape( 110, 500, 930, 500 )
+	border.bottom = {}
+	border.bottom.body = love.physics.newBody(world, 520, 505, "static")
+	border.bottom.shape = love.physics.newRectangleShape(820, 10)
+	border.bottom.fixture = love.physics.newFixture(border.bottom.body, border.bottom.shape, 10) -- A higher density gives it more mass.
+	
+	--left = love.physics.newEdgeShape( 120, 90, 120, 510 )
+	border.left = {}
+	border.left.body = love.physics.newBody(world, 115, 300, "static")
+	border.left.shape = love.physics.newRectangleShape(10, 410)
+	border.left.fixture = love.physics.newFixture(border.left.body, border.left.shape, 10) -- A higher density gives it more mass.
+	
+	--right = love.physics.newEdgeShape( 920, 90, 920, 510 )
+	border.right = {}
+	border.right.body = love.physics.newBody(world, 925, 300, "static")
+	border.right.shape = love.physics.newRectangleShape(10, 410)
+	border.right.fixture = love.physics.newFixture(border.right.body, border.right.shape, 10) -- A higher density gives it more mass.
+
+	player = love.graphics.newImage("media/personsmall.png")
 	px = 1025/2
 	py = 600/2
-	pspeed = 100
+	pspeed = 200
 	playerrot = 0
+	pbody = love.physics.newBody( world, px , py, "dynamic" )
+	pshape = love.physics.newCircleShape(12)
+	pfix = love.physics.newFixture(pbody, pshape, 3)
 	
 	ymax = 490
 	ymin = 100
@@ -19,82 +51,87 @@ function startgame()
 	spxmax = 870
 	spxmin = 150
 	
-	bodies = 0
+	bodies = 500
+	
+	mouse_x = love.mouse.getX()
+	mouse_y = love.mouse.getY()
+	
 	
 	function love.update(dt)
+		world:update(dt)
 		if love.keyboard.isDown("up") and love.keyboard.isDown("left") then
-			if py < ymin or px < xmin then
-			else
-				playerrot = -0.785398163
-				py = py - (pspeed *dt)
-				px = px - (pspeed *dt)
-			end
+			playerrot = -0.785398163
+			--pbody:setLinearVelocity( 0, 0 )
+			pbody:applyForce( -1*(pspeed/2), -1*(pspeed/2))
 		elseif love.keyboard.isDown("up") and love.keyboard.isDown("right") then
-			if py < ymin or px > xmax then
-			else
-				playerrot = 0.785398163
-				py = py - (pspeed *dt)
-				px = px + (pspeed *dt)
-			end
+			playerrot = 0.785398163
+			--pbody:setLinearVelocity( 0, 0 )
+			pbody:applyForce( pspeed/2, -1*(pspeed/2))
 		elseif love.keyboard.isDown("down") and love.keyboard.isDown("left") then
-			if py > ymax or px < xmin then
-			else
-				playerrot = -2.35619449
-				py = py + (pspeed *dt)
-				px = px - (pspeed *dt)
-			end
+			playerrot = -2.35619449
+			--pbody:setLinearVelocity( 0, 0 )
+			pbody:applyForce( -1*(pspeed/2), pspeed/2)
+				
 		elseif love.keyboard.isDown("down") and love.keyboard.isDown("right") then
-			if py > ymax or px > xmax then
-			else
-				playerrot = 2.35619449
-				py = py + (pspeed *dt)
-				px = px + (pspeed *dt)
-			end
+
+			playerrot = 2.35619449
+			--pbody:setLinearVelocity( 0, 0 )
+			pbody:applyForce( pspeed/2, pspeed/2)
 		else
 			if love.keyboard.isDown("left") then
-			  if px<xmin then
-			  else
 				playerrot =(-1.57079633)
-				px = px - (pspeed *dt)
-			  end
+				--pbody:setLinearVelocity( 0, 0 )
+				pbody:applyForce( -1*(pspeed), 0)
+				
 		   elseif love.keyboard.isDown("right") then
-			  if px>xmax then
-			  else
 				playerrot =(1.57079633)
-				px = px + (pspeed *dt)
-			  end
+				--pbody:setLinearVelocity( 0, 0 )
+				pbody:applyForce( pspeed, 0)
 
 		   elseif love.keyboard.isDown("up") then
-			  if py < ymin then
-			  else
 				playerrot =(0)
-				py = py - (pspeed *dt)
-			  end
+				--pbody:setLinearVelocity( 0, 0 )
+				pbody:applyForce( 0, -1*(pspeed))
 		   elseif love.keyboard.isDown("down") then
-			  if py>ymax then
-			  else
 				playerrot =(3.14159265)
-				py = py + (pspeed *dt)
-			  end
+				--pbody:setLinearVelocity( 0, 0 )
+				pbody:applyForce( 0, pspeed)
 		   end
 		end
 	end
 	function love.draw()
+	
 	  --love.graphics.setColor( 0, 0, 0, 255)
 	  --love.graphics.rectangle("fill", 0, 0, 1025, 600)
 	  love.graphics.setColor( 255, 255, 255, 255)
-	  love.graphics.print("FPS: "..love.timer.getFPS(), 5, 0)
-	  love.graphics.print("EGO", 1025/2,0)
+	  
 	  love.graphics.setColor( 75, 75, 75, 255)
 	  love.graphics.rectangle("fill", 0, 35, 1025, 565)
+	  
 	  love.graphics.setColor( 125, 125, 125, 255)
 	  love.graphics.rectangle("fill", 120, 100, 800, 400)
 	  
-	  spawnCrowd(bodies)
+	  love.graphics.setColor( 0, 0, 0, 255)
+	  love.graphics.polygon("fill", border.top.body:getWorldPoints(border.top.shape:getPoints()))
+	  love.graphics.polygon("fill", border.bottom.body:getWorldPoints(border.bottom.shape:getPoints()))
+	  love.graphics.polygon("fill", border.left.body:getWorldPoints(border.left.shape:getPoints()))
+	  love.graphics.polygon("fill", border.right.body:getWorldPoints(border.right.shape:getPoints()))
+	  	 
+	  love.graphics.reset()
+	  draw(bodies)
 	  
-	  love.graphics.draw(player, px, py, playerrot, .33, .33, 32, 32)
+	  love.graphics.setColor( 0, 255, 0, 255)
+	  --pbody:setX(px)
+	  --pbody:setY(py)
+	  love.graphics.circle("fill", pbody:getX(), pbody:getY(), pshape:getRadius())
+	  love.graphics.reset()
+	  love.graphics.draw(player, pbody:getX(), pbody:getY(), playerrot, 1, 1, 8, 8)
+	  
+	  love.graphics.draw(scanlines, 0, 0)
+	  love.graphics.print("FPS: "..love.timer.getFPS(), 5, 0)
+	  love.graphics.print("EGO", 1025/2,0)
+	  love.graphics.print( "Mouse X: ".. love.mouse.getX() .. " Mouse Y: " .. love.mouse.getY(), 600, 0)
 
-	  
 	  
 	end
 
